fe249ebf98fb9ea1fdf3bea0e9c8243dc2950330
Don Han <don_han@berkeley.edu>
2016-02-06 23:11:05 -0800

    dotfiles are located within dotfiles folder

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,29 +4,47 @@
 
 ########## Variables ########## 
 
-DIR=`pwd`
+DIR=`pwd`/dotfiles          # location of dotfiles
+if [ ! -d "$DIR" ]; then
+    echo "$DIR does not exist. Exiting..."
+    exit
+fi
+
 olddir=old_dotfiles         # old dotfiles backup directory
-FILES=`ls | grep -v '.sh$' | grep -v $olddir` # Do not display files that end in .sh or $olddir
+FILES=`ls $DIR`
+#FILES=`ls $DIR | grep -v '.sh$' | grep -v $olddir` # Do not display files that end in .sh or $olddir
 
 ###############################
-
 for FILE in $FILES; do
-    if [ -e ~/.$FILE ] ; then
-        if [ ! -d $olddir ]; then
-            echo "[*] Creating a directory '$olddir' for backup of any existing dotfiles"
-            mkdir -p $olddir
+    if [ ! -d $olddir ]; then
+        echo "[*] Creating a directory '$olddir' for backup of any existing dotfiles"
+        mkdir -p $olddir
+    fi
+
+    # if symlink
+    if [ -L ~/.$FILE ]; then
+        echo "[*] Removing old symlink of '$FILE'"
+        unlink ~/.$FILE
+    # if file or directory
+    elif [ -e ~/.$FILE ]; then
+        echo "[*] Backing up '$FILE' to the '$olddir'"
+
+        # weird behavior for mv when moving directory
+        # if directory
+        if [ -d ~/.$FILE ]; then
+            rm -rf $olddir/.$FILE
         fi
-        echo "[*] Moving already existing dotfile '$FILE' to the '$olddir'"
         mv ~/.$FILE $olddir
     fi
+
     echo "[*] Creating symlink to $FILE in home directory."
     ln -s $DIR/$FILE ~/.$FILE
 done
 
 # this removes the dot from dotfiles in $olddir; to re-add dot, use prefix_dot.sh file
-#rename -f 's/\.//' $olddir/.[^.]*
+rename -f 's/\.//' $olddir/.[^.]*
 
-#mkdir -p ~/.vim/{backup,swap,undo} # setup vimrc
+mkdir -p ~/.vim/{backup,swap,undo} # setup vimrc
 
 source ~/.bashrc
 
